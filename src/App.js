4b4eac54f693a1dde5f081b61880b0fbc7d78449
Nazar Diachok <diafcc55@gmail.com>
2022-12-04 12:11:49 +0100

    -react-router-dom installed -Navigation created -Navigation to App.js added, not styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import objekte from "./objekte.json";
 import './App.css';
+import Navigation from "./Navigation/Navigation";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation></Navigation>
+      <Routes>
+        <Route path="/angebote" >
+
+        </Route>
+        <Route path="/kontakte" >
+
+        </Route>
+      </Routes>
     </div>
   );
 }
